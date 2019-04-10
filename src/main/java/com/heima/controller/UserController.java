98d8e604a89fe926802513f10eda656f2448d6ab
982541919 <982541919@qq.com>
2019-04-10 23:44:52 +0800

    Update UserController.java

--- src/main/java/com/heima/controller/UserController.java
+++ src/main/java/com/heima/controller/UserController.java
@@ -14,7 +14,7 @@ public class UserController {
 	public String getString() { 
 		return restTemplate.getForObject("http://heima-provider-1/getString", String.class);
 	}	
-	//7
+	//1
 	@RequestMapping("getMap")
 	public String getMap() {
 		// 6 
