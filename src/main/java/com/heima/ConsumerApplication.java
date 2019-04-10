8382bbe2f2979e58a2946bd27ca81ca281d09b7c
982541919 <982541919@qq.com>
2019-04-10 20:54:45 +0800

    提交消费者

---
+++ src/main/java/com/heima/ConsumerApplication.java
@@ -0,0 +1,25 @@
+package com.heima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@EnableEurekaClient
+public class ConsumerApplication {
+	
+	@Bean // 该注解类似于spring中的 <bean>标签，会在启动的时候执行改方法  
+	@LoadBalanced // 加上该注解使用rebbon，让RestTemplate具备rebbon的负载均衡能力
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/heima/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.heima.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class UserController {
+	@Autowired
+	RestTemplate restTemplate;
+
+	@RequestMapping("getString")
+	public String getString() { 
+		return restTemplate.getForObject("http://heima-provider-1/getString", String.class);
+	}
+	
+	@RequestMapping("getMap")
+	public String getMap() {
+		return restTemplate.getForObject("http://heima-provider-1/getMap", String.class);
+	}
+
+}
